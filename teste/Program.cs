13ca0ab5dd9c57238000433f4b2d59fd3db23d35
Scapin-Raul <raul.scapin19@gmail.com>
2019-04-14 17:42:38 -0300

    terminado o teste

--- teste/Program.cs
+++ teste/Program.cs
@@ -5,24 +5,68 @@ namespace teste
     class Program
     {
         static void Main(string[] args)
-        {
-
-                    // System.Console.WriteLine("     ....    ....");
-                    // System.Console.WriteLine("   .     . .      .");
-                    // System.Console.WriteLine("  .      . .       .");
-                    // System.Console.WriteLine(" .        .         .");
-                    // System.Console.WriteLine(" .                  .");
-                    // System.Console.WriteLine("  .                .");
-                    // System.Console.WriteLine("   .              .");
-                    // System.Console.WriteLine("    .            .");
-                    // System.Console.WriteLine("      .        .");
-                    // System.Console.WriteLine("        .    .");
-                    // System.Console.WriteLine("          . .");
-                    // System.Console.WriteLine("           .");
-
-
-
-                    
+        
+        {           string resposta;
+                    System.Console.WriteLine();
+                    // System.Console.WriteLine("Bem vindo ao pedidor de namoro.");
+                    System.Console.WriteLine("Para que o programa possa ser inicializado com eficiencia");
+                    System.Console.Write("Insira seu nome: ");
+                    string nome = Console.ReadLine();
+                    System.Console.WriteLine();
+                    System.Console.Write($"Olá {nome}. ");
+                    System.Threading.Thread.Sleep(500);
+                    System.Console.Write("Será que você não quer namorar o ");
+                    System.Console.Write("R");
+                    System.Threading.Thread.Sleep(100);
+                    System.Console.Write("a");
+                    System.Threading.Thread.Sleep(100);
+                    System.Console.Write("u");
+                    System.Threading.Thread.Sleep(100);
+                    System.Console.Write("l");
+                    System.Threading.Thread.Sleep(100);
+                    System.Console.Write("zi");
+                    System.Threading.Thread.Sleep(100);
+                    System.Console.Write("nho");
+                    System.Threading.Thread.Sleep(500);
+                    System.Console.WriteLine("?");
+                    System.Threading.Thread.Sleep(1000);
+                    do
+                    {
+                        System.Console.WriteLine($"\nDigite a resposta? (sim ou nao)");
+                        resposta = Console.ReadLine();  
+                    } while (!"sim".Equals(resposta)&&!"nao".Equals(resposta));
+                    if (resposta.Equals("sim")){
+                        System.Console.WriteLine("");
+                        System.Console.WriteLine("     ****   ****");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("    *    * *    *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("   *     * *     *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("  *       *       *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine(" *     Parabens    *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("  *     agora     *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("   *   você me   *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("     *  namora *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("      *  kkk  *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("        *   *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("         * *");
+                        System.Threading.Thread.Sleep(300);
+                        System.Console.WriteLine("          *");
+                        System.Threading.Thread.Sleep(1000);
+                        System.Console.WriteLine("\nPrograma feito by: Vakkuzinho");
+                    }else
+                    {
+                        System.Console.WriteLine("Hum, que pena, mas tudo bem!");
+                        System.Console.WriteLine("\nPrograma feito by: Vakkuzinho");
+                    }
         }
     }
 }
---
+++ teste/sdasdaasdsda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace teste
+{
+    class sdasdaasdsda
+    {
+        
+    //     static void Main(string[] args)
+    //     {
+    //                 System.Console.WriteLine("     ****   ****");
+    //                 System.Console.WriteLine("    *    * *    *");
+    //                 System.Console.WriteLine("   *     * *     *");
+    //                 System.Console.WriteLine("  *       *       *");
+    //                 System.Console.WriteLine(" *     Parabens    *");
+    //                 System.Console.WriteLine("  *     agora     *");
+    //                 System.Console.WriteLine("   *   você me   *");
+    //                 System.Console.WriteLine("     *  namora *");
+    //                 System.Console.WriteLine("      *  kkk  *");
+    //                 System.Console.WriteLine("        *   *");
+    //                 System.Console.WriteLine("         * *");
+    //                 System.Console.WriteLine("          *");
+
+    //     }
+    }
+}
